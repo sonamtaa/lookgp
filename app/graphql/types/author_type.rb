0349b573465a3082a159675e375a3a545e43a499
sonamtaahee <tashico2016@gmail.com>
2021-07-02 13:58:43 +0600

    initial commit

---
+++ app/graphql/types/author_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class AuthorType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :yob, Integer, null: false
+    field :is_alive, Boolean, null: false
+  end
+end
---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,27 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+
+    def test_field
+      "Hello World!"
+    end
+
+    field :author, Types::AuthorType, null: true,
+      description: "Returns the Author with the given id" do
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.find_by(id: id)
+    end
+  end
+end
