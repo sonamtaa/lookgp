ba54f48852d1e2c7a28361982c38aaad701c2c2c
sonamtaahee <tashico2016@gmail.com>
2021-07-02 14:08:55 +0600

    rubocop offences with -A

--- app/graphql/lookgq_schema.rb
+++ app/graphql/lookgq_schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class LookgqSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Types
   class AuthorType < Types::BaseObject
     field :id, ID, null: false
